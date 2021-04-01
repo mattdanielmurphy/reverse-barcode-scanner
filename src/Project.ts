b89505c3d5cfcf88e60f78fb2f8c4b1f6e6670ac
Matthew Murphy <matt@mattmurphy.ca>
2021-03-31 19:29:17 -0600

    move Project class into its own file

---
+++ src/Project.ts
@@ -0,0 +1,135 @@
+//
+// * IMPORTS
+import editJsonFile = require('edit-json-file')
+
+import * as path from 'path'
+
+import { exec } from 'child_process'
+
+interface ShellCommandOptions {
+	cwd?: string
+	verboseMode?: boolean
+}
+
+export class Project {
+	projectDirectory: string
+	constructor(projectName: string) {
+		const workingDirectory = path.resolve(__dirname, '../..')
+		this.projectDirectory = path.resolve(workingDirectory, projectName)
+
+		const commands = [
+			{
+				message: `Cloning repo into folder '${projectName}'`,
+				command: `gh repo clone mattdanielmurphy/create-node-project ${projectName}`,
+				options: { cwd: workingDirectory },
+			},
+			{
+				message: 'Removing installer files',
+				command: 'rm -r src',
+			},
+			{
+				message: 'Updating package.json',
+				fn: () => this.updatePackageJSON(projectName),
+			},
+			{
+				message: 'Updating readme',
+				command: `echo "# ${projectName}" > readme.md`,
+			},
+			{
+				message: 'Creating GitHub repo',
+				command: `git remote remove origin; gh repo create ${projectName} -y --public`,
+			},
+			{
+				message: 'Pushing first commit',
+				command:
+					'git add .; git commit -m "initial commit"; git push -u origin main',
+			},
+			{
+				message: 'Installing packages',
+				command:
+					'mkdir node_modules.nosync; ln -s node_modules.nosync node_modules; yarn',
+			},
+			{
+				message: 'Opening project folder in Visual Studio Code',
+				command: 'open . -a Visual\\ Studio\\ Code\\ -\\ Insiders',
+			},
+		]
+
+		console.log('All done!')
+		this.executeListOfShellCommands(commands)
+	}
+	updatePackageJSON(projectName: string): void {
+		const file = editJsonFile(path.join(this.projectDirectory, 'package.json'))
+		file.set('name', projectName)
+		file.set('version', '0.0.1')
+		file.set('repository', `git@github.com:mattdanielmurphy/${projectName}.git`)
+		file.save()
+	}
+	async executeListOfShellCommands(
+		commands: {
+			message: string
+			command?: string
+			fn?: () => void
+			options?: ShellCommandOptions
+		}[],
+		options?: ShellCommandOptions,
+	): Promise<void> {
+		const optionsForAllCommands = options || {}
+		let lastCommandFailed = false
+
+		for (let i = 0; i < commands.length; i++) {
+			const { message, command, options = {}, fn } = commands[i]
+			Object.assign(options, optionsForAllCommands)
+
+			if (lastCommandFailed) return
+
+			console.log(`[${i + 1}/${commands.length}] ${message}...`) // [1/7] Cloning Repo...
+			if (fn) {
+				try {
+					fn()
+				} catch (error) {
+					console.log(error)
+					lastCommandFailed = true
+				}
+			} else {
+				await this.executeShellCommand(command || '', options).catch(
+					() => (lastCommandFailed = true),
+				)
+			}
+		}
+	}
+	async executeShellCommand(
+		command: string,
+		options: ShellCommandOptions = {},
+	): Promise<string | Buffer> {
+		let verboseMode: boolean
+		if (options.verboseMode) {
+			delete options.verboseMode
+			verboseMode = true
+		}
+
+		options = {
+			cwd: this.projectDirectory,
+			...options,
+		}
+
+		return new Promise((resolve, reject) => {
+			exec(
+				command,
+				options,
+				(
+					error: Error | null,
+					stdout: string | Buffer,
+					stderr: string | Buffer,
+				) => {
+					if (error) {
+						console.log(error)
+						reject(error)
+					}
+					if (verboseMode && stdout) console.log(stdout)
+					resolve(stdout ? stdout : stderr)
+				},
+			)
+		})
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,143 +1,22 @@
-//
-// * IMPORTS
-
-import editJsonFile = require('edit-json-file')
-
-import * as path from 'path'
-
-import { exec } from 'child_process'
-
-//
-// * PROGRAM
-
-const projectName = process.argv.slice(2).join('-')
-
-if (projectName) createProject(projectName)
-else
-	console.log(
-		'Please provide a project name:\n\tcreate-node-project [directory] [project-name]',
-	)
+import { Project } from './Project'
 
 //
 // * FUNCTIONS
 
-interface Options {
-	cwd?: string
-	verboseMode?: boolean
-}
+function getProjectNameFromParameters(): string {
+	// $ create-node-project [name of project with spaces]
+	const projectName = process.argv.slice(2).join('-') // name-of-project-with-spaces
 
-async function executeShellCommand(command: string, options: Options = {}) {
-	let verboseMode: boolean
-	if (options.verboseMode) {
-		delete options.verboseMode
-		verboseMode = true
-	}
-
-	return new Promise((resolve, reject) => {
-		exec(
-			command,
-			options,
-			(
-				error: Error | null,
-				stdout: string | Buffer,
-				stderr: string | Buffer,
-			) => {
-				if (error) {
-					console.log(error)
-					reject(error)
-				}
-				if (verboseMode && stdout) console.log(stdout)
-				resolve(stdout ? stdout : stderr)
-			},
+	if (projectName) {
+		return projectName
+	} else
+		throw new Error(
+			'Please provide a project name:\n\tcreate-node-project [directory] [project-name]',
 		)
-	})
 }
 
-async function createProject(projectName: string) {
-	const workingDirectory = path.resolve(__dirname, '../..')
-	const projectDirectory = path.resolve(workingDirectory, projectName)
-	async function executeListOfShellCommands(
-		commands: {
-			message: string
-			command?: string
-			fn?(): void
-			options?: Options
-		}[],
-		options?: Options,
-	) {
-		const optionsForAllCommands = options || {}
-		let lastCommandFailed = false
+//
+// * PROGRAM START
 
-		for (let i = 0; i < commands.length; i++) {
-			const { message, command, options = {}, fn } = commands[i]
-			Object.assign(options, optionsForAllCommands)
-
-			if (lastCommandFailed) return
-
-			console.log(`[${i + 1}/${commands.length}] ${message}...`) // [1/7] Cloning Repo...
-			if (fn) {
-				try {
-					fn()
-				} catch (error) {
-					console.log(error)
-					lastCommandFailed = true
-				}
-			} else {
-				await executeShellCommand(command || '', {
-					cwd: projectDirectory,
-					...options,
-				}).catch(() => (lastCommandFailed = true))
-			}
-		}
-	}
-
-	const commands = [
-		{
-			message: `Cloning repo into folder '${projectName}'`,
-			command: `gh repo clone mattdanielmurphy/create-node-project ${projectName}`,
-			options: { cwd: workingDirectory },
-		},
-		{
-			message: 'Removing installer files',
-			command: 'rm -r src',
-		},
-		{
-			message: 'Updating package.json',
-			fn: () => {
-				const file = editJsonFile(path.join(projectDirectory, 'package.json'))
-				file.set('name', projectName)
-				file.set('version', '0.0.1')
-				file.set(
-					'repository',
-					`git@github.com:mattdanielmurphy/${projectName}.git`,
-				)
-				file.save()
-			},
-		},
-		{
-			message: 'Updating readme',
-			command: `echo "# ${projectName}" > readme.md`,
-		},
-		{
-			message: 'Creating GitHub repo',
-			command: `git remote remove origin; gh repo create ${projectName} -y --public`,
-		},
-		{
-			message: 'Pushing first commit',
-			command:
-				'git add .; git commit -m "initial commit"; git push -u origin main',
-		},
-		{
-			message: 'Installing packages', // and do node_modules.nosync trick
-			command:
-				'mkdir node_modules.nosync; ln -s node_modules.nosync node_modules; yarn',
-		},
-		{
-			message: 'Opening project folder in Visual Studio Code',
-			command: 'open . -a Visual\\ Studio\\ Code\\ -\\ Insiders',
-		},
-	]
-
-	console.log('All done!')
-	executeListOfShellCommands(commands)
-}
+const projectName = getProjectNameFromParameters()
+new Project(projectName)
